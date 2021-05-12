ad9c094f22230a6a0ac6de3f2029cf37e3df1a0f
Monica261 <desenvolvimento.midiasim@gmail.com>
2021-05-11 21:37:39 -0300

    deixando a navegação mais fluida sem precisar recarregar a pág

--- JAVASCRIPT_ASSINCRONO/history-api/script.js
+++ JAVASCRIPT_ASSINCRONO/history-api/script.js
@@ -1,23 +1,24 @@
 console.log(window.history);//mostra por quantos itens já navegamos atraves do
 //lenght
 
-//console.log(window.history.back());//ele volta pra pág anterior
+console.log(window.history.back());//ele volta pra pág anterior
 
-//console.log(window.history.forward());
+console.log(window.history.forward());//vai pra prox pág
 
 //pushState eu posso colocar algo no estado do History, mudar a minha url
-//console.log(window.history.pushState(null, null, 'teste.html'));
+console.log(window.history.pushState(null, null, 'teste.html'));
 
 
 //popState fica de olho no botão voltar ou ir pra frente, quando isso ocorrer
 //ele é acionado
 
 //se eu não modificar o pushState, o popState não funciona
-//window.addEventListener('popstate', ()=>{
-//    console.log('teste');
-//})
+window.addEventListener('popstate', ()=>{
+    console.log('teste');
+})
 
 //Fetch e history
+//deixando a navegação mais fluida sem precisar recarregar
 const links = document.querySelectorAll('a');
 
 links.forEach(link =>{
@@ -27,11 +28,27 @@ links.forEach(link =>{
 function handleClick(event){
     event.preventDefault();
     fetchPage(event.target.href);
+    window.history.pushState(null, null, event.target.href);
 }
 
 async function fetchPage(url){
     const pageResponse = await fetch(url);
     const pageText = await pageResponse.text();
-    console.log(pageText);
+    replaceContent(pageText);
 }
 fetchPage();
+
+function replaceContent(newText){
+    const newHTML = document.createElement('div');
+    newHTML.innerHTML = newText;
+
+    const oldContent = document.querySelector('.content');
+    const newContent = newHTML.querySelector('.content');
+
+    oldContent.innerHTML = newContent.innerHTML;
+    document.title = newHTML.querySelector('title').innerText;
+}
+
+window.addEventListener('popstate',() =>{
+    fetchPage(window.location.href)
+})
