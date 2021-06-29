8edc492d6919c1d9548bd2b7bfae39952f1a8389
Monica261 <desenvolvimento.midiasim@gmail.com>
2021-06-29 20:35:56 -0300

    aprendedo sobre propriedades e metodos de classes em js

--- CLASSES/classes/script.js
+++ CLASSES/classes/script.js
@@ -1,53 +1,64 @@
-//constructor function é responsavel por criação de objeto
+//CONSTRUCTOR FUNCTION:
+
 function Button(text, background){
-    this.text = text;
+	this.text = text;
     this.background = background;
 }
 
 Button.prototype.element = function(){
-    const btnelemento = document.createElement('button');
-    btnelemento.innerText = this.text;
-    btnelemento.style.background = this.background;
-    return btnelemento;
-    
+	const buttonElement = document.createElement('button');
+    buttonElement.innerText = this.text;
+    buttonElement.style.background = this.background;
+    return buttonElement;
 }
 
-const redButton = new Button('comprar', 'red');
-console.log(redButton)
-
-//Classes são syntactical sugar de construtor functions, sintaxe mais limpa 
-//This é a palavra chave, this faz referencia ao objeto construido a partir do New
-
-class Carro {
-    constructor(modelo, background, width, height){
-        this.modelo = modelo;
+//CLASS:
+//window.onload = () => {//para que o erro de appendChild não ocorra mais
+class Button{
+	constructor(text, background){
+    	this.text = text;
         this.background = background;
-        this.width = width;
-        this.height = height;
     }
     element(){
-        const caracCarro = document.createElement('div');
-        caracCarro.innerText = this.modelo;
-        caracCarro.style.background = this.background;
-        caracCarro.style.width = this.width + 'px';
-        caracCarro.style.height = this.height + 'px';
-        return caracCarro;
+   	const buttonElement = document.createElement('button');
+        buttonElement.innerText = this.text;
+        buttonElement.style.background = this.background;
+        return buttonElement;
     }
-    appendTo(target){//metodo pra colocar no body
-        const targetElement = document.querySelector(target);
+    appendTo(target){//injetar no body o element
+   	const targetElement = document.querySelector(target);
         targetElement.appendChild(this.element());
         return targetElement;
     }
-    static purpleCar(text){//retorna a criação da propria classe,com algo pré passado
-        return new Carro(text, 'blue', 90, 70)
+}
+
+
+//const blueButton = new Button('Clique', 'blue');//essa const é um obj
+//console.log(blueButton.appendTo('body'));
+//}
+
+//class é considerado uma syntetical sugar
+
+//PROPRIEDADES:
+//eu posso passar as opções direto
+
+class Button{
+	constructor(options){
+    	this.options = options;
     }
 }
 
-const carroPurple = Carro.purpleCar('Ford');
-console.log(carroPurple);
+const optionsPink = {
+	backgroundColor: 'pink',
+    text: 'Comprar',
+    color: 'white'
+}
 
-const ferrari = new Carro('ferrari', 'red', 40, 40);
-console.log(ferrari.appendTo('body'))
+const pinkButton = new Button(optionsPink);
+
+//STATIC
+
+
 
 //Uma propriedade pode ter qualquer valor
 
