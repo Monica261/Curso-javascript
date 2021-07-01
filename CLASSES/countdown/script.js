7543cec7c802f36cff7833b53ce21c30a76f648d
Monica261 <desenvolvimento.midiasim@gmail.com>
2021-06-30 21:28:05 -0300

    plugin criado para contagem regressiva

--- CLASSES/countdown/script.js
+++ CLASSES/countdown/script.js
@@ -4,3 +4,5 @@ const tempoParaONatal = new Countdown('24 December 2021 23:59:59 GMT-0300');
 
 console.log(`Faltam ${tempoParaONatal.days} dias para o natal`);
 console.log(tempoParaONatal.total);//o obj tem dias, horas, minutos e segundos
+
+const body = document.body.innerText = tempoParaONatal.total;
