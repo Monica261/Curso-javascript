1ac1937534e9ff72c04ddb4e76ce1566fe6a3ecb
Monica261 <desenvolvimento.midiasim@gmail.com>
2021-06-30 22:04:59 -0300

    Injetando objt do modulo

--- CLASSES/countdown/script.js
+++ CLASSES/countdown/script.js
@@ -5,4 +5,7 @@ const tempoParaONatal = new Countdown('24 December 2021 23:59:59 GMT-0300');
 console.log(`Faltam ${tempoParaONatal.days} dias para o natal`);
 console.log(tempoParaONatal.total);//o obj tem dias, horas, minutos e segundos
 
-const body = document.body.innerText = tempoParaONatal.total;
+const container = document.querySelector('.container');
+container.innerHTML += `
+    <p>Faltam ${tempoParaONatal.total.days} dias ${tempoParaONatal.total.hours} horas para o natal</p>
+`
