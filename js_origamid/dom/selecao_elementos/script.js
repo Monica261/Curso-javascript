dd3673ced21a91eb36b366ab82ae6d11383efb4d
Monica261 <desenvolvimento.midiasim@gmail.com>
2022-02-06 18:41:12 -0300

    selecionando elementos de diversas formas

--- js_origamid/dom/selecao_elementos/script.js
+++ js_origamid/dom/selecao_elementos/script.js
@@ -12,6 +12,25 @@ console.log(grid.baseURI);
 //selecionar todas as urls internas
 const links= document.querySelectorAll('[href^="#"]')
 
+//htmlcollection e nodelist
+const gridSectionHTML  = document.getElementsByClassName('grid-section');
+const gridSectionNODE = document.querySelectorAll('.grid-section');
+
+console.log(gridSectionHTML[0])
+console.log(gridSectionNODE[0])
+
+//transformando em array like
+gridSectionNODE.forEach(function(item, index){
+  console.log(index, item)
+})
+
+//tranforma tudo que é array like em array
+const arrayGrid = Array.from(gridSectionHTML);
+
+//após transformar oq era array like em array eu posso usar o forEach
+arrayGrid.forEach(function(item){
+  console.log(item)
+})
 
 //array like
 const gridSection = document.querySelectorAll('.grid-section');
--- js_origamid/dom/selecao_elementos/script_exer.js
+++ js_origamid/dom/selecao_elementos/script_exer.js
@@ -3,6 +3,8 @@ const imagens = document.querySelectorAll('img');
 console.log(imagens);
 
 // Retorne no console apenas as imagens que começaram com a palavra imagem
+const somentImg = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(somentImg);
 
 // Selecione todos os links internos (onde o href começa com #)
 const linksInternos = document.querySelectorAll('[href^="#"]')
@@ -13,6 +15,6 @@ const animaisDescricao = document.querySelector('.animais-descricao h2');
 console.log(animaisDescricao.innerHTML)
 
 // Selecione o último p do site
-const ultimoP = document.querySelector('p');
+const ultimoP = document.querySelectorAll('p');
 
-console.log()
+console.log(ultimoP[ultimoP.length -1]) //tras o ultimo p baseado na quantidade
